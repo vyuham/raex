adb2973b46213839155162f06de2a159aa5c4e63
Devdutt Shenoi <devdutt@outlook.in>
2020-12-02 19:20:02 +0530

    Add test for configuration

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use config::{Config, File};
 use serde::{Serialize, Deserialize};
 
 /// A configuration module to handle details of pre-configured cluster
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct RaexConfig {
     /// URL values used in locating nodes, used in GRPC comm.
     nodes: Vec<String>,
@@ -14,10 +14,13 @@ impl RaexConfig {
     pub fn load() -> Self {
         let mut settings = Config::default();
         // The target file to load configuration from is raex.toml
-        settings.merge(File::with_name("raex"));
-        Self {
-            nodes: settings.get("nodes").unwrap(),
-            global: settings.get("global").unwrap()
+        if let Err(e) = settings.merge(File::with_name("raex")) {
+            eprintln!("Error: {}", e);
+            panic!("Can't configure node.")
+        } else {
+            let nodes: Vec<String> = settings.get::<Vec<String>>("nodes").unwrap();
+            let global: String = settings.get::<String>("global").unwrap();
+            Self { nodes, global }
         }
     }
 
@@ -25,3 +28,16 @@ impl RaexConfig {
     pub fn global(self) -> String { self.global }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn load_config_test() {
+        let loaded_config = RaexConfig::load();
+        let expected = RaexConfig {
+            nodes: vec!["0.0.0.0".to_string()], 
+            global: "0.0.0.0".to_string()
+        };
+        assert_eq!(loaded_config, expected);
+    }
+}
