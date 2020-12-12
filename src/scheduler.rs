7fdd17315fe729c54a6daadd23fea58bec704664
Devdutt Shenoi <devdutt@localhost.localdomain>
2020-12-12 16:38:20 +0530

    Refactor scheduler logic to describe creation of executor node and executing scheduled processes, write test for scheduling

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,31 +1,76 @@
-use crate::executor::Exec;
-use std::collections::VecDeque;
+use crate::executor::{Exec, Task, Data, Color};
+use std::collections::{VecDeque, HashMap};
 
 /// Maintains schedule within raft consensus.
 pub struct Sched {
     /// A queue of processes waiting to be executed.
     scheduled: VecDeque<Exec>,
     /// A list of processes being executed.
-    executing: Vec<Exec>,
+    executing: HashMap<u8,Option<Exec>>,
     /// A log of all processes that have finished executing.
-    completed: Vec<Exec>
+    completed: Vec<Exec>,
 }
 
 impl Sched {
     pub fn default() -> Self {
         Self {
             scheduled: VecDeque::new(),
-            executing: vec![],
-            completed: vec![]
+            executing: HashMap::new(),
+            completed: vec![],
         }
     }
 
-    pub fn next(&mut self) {
-        if !self.scheduled.is_empty() {
-            let exec = self.scheduled.pop_front().unwrap();
-            self.executing.push(exec);
-        } else {
-            panic!()
+    pub fn schedule(&mut self, exec: Exec) {
+        self.scheduled.push_back(exec);
+    }
+
+    pub fn add_executor_node(&mut self) -> u8 {
+        let mut node_id: u8 = 0;
+        for i in 0..u8::MAX {
+            // executing.get() -> Option<Option<Exec>>, so Some(None) implies the node exists
+            if self.executing.get(&i).is_none() {
+                self.executing.insert(i, None);
+                node_id = i;
+                break;
+            }
         }
+        node_id
+    }
+
+    pub fn node_status(&self) -> String {
+        let mut output = "".to_string();
+        for (node, exec) in self.executing.clone() {
+            output.push_str(&format!("Node #{}: Running process {}", node, match exec {
+                Some(exec) => exec.in_words(),
+                None => "Nothing".to_string()
+            }))
+        }
+        output
+    }
+
+    pub fn next(&mut self) {
+        for (node, exec) in self.executing.clone() {
+            match (exec, self.scheduled.pop_front()) {
+                (None, Some(exec)) => {
+                    self.executing.insert(node,Some(exec));
+                },
+                (None, _) => panic!("No process scheduled"),
+                _ => panic!("Couldn't find a free node"),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_scheduling() {
+        let mut sched = Sched::default();
+        let node_id = sched.add_executor_node();
+        let exec = Exec::new(Task::Divide, Data::Image(vec![vec![Color::new(1,1,1)]]));
+        sched.schedule(exec.clone());
+        sched.next();
+        assert_eq!(sched.node_status(), format!("Node #{}: Running process {}", node_id, exec.in_words()));
     }
 }
